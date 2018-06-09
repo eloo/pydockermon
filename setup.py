f30252869af36a3d30dfb216ae5287dc11b72f5b
Joakim Sørensen <joasoe@gmail.com>
2018-06-09 09:03:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="pydockermon",
-    version="0.0.1",
+    version="0.0.2",
     author="Joakim Sorensen",
     author_email="joasoe@gmail.com",
     description="A python module to interact with ha-dockermon.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ludeeus/pydockermon",
+    url="https://gitlab.com/ludeeus/pydockermon",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
